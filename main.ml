a63d870a777242a882780b57f954e6b2c1dbedb8
Chiaki ISHIO <g1420505@is.ocha.ac.jp>
2017-10-28 22:03:19 +0900

    task3b: prep

--- main.ml
+++ main.ml
@@ -5,6 +5,7 @@ let go () =
   let kprogram = Knormal.f program in	(* k-正規形に変換し、*)
   let fprogram = First.f kprogram in	(* １階の言語に変換し、*)
   let fprogram = Prealloc.f fprogram in	(* レジスタ割り当て前処理を行い、*)
+  let fprogram = Alloc.f fprogram in	(* レジスタ割り当てを行い、*)
   First.print fprogram			(* 表示する。*)
 
 (* スタートアップ *)
